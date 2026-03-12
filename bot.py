7d72022efe85f46a802b781a6e805ce03264cbef
lucian-xinitrc <cusmirlucian@gmail.com>
2026-03-12 14:41:09 +0000

    New commit

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ from disnake.ext import commands
 from disnake import TextInputStyle
 
 load_dotenv()
-
 bot = commands.Bot(
 	intents=disnake.Intents.all(), 
 	allowed_mentions=disnake.AllowedMentions(everyone=True)
